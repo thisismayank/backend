84c21dd55cc0ac90fb2b162ed7174dc1fd6497a4
Mayank Arya <mayank@getbasis.co>
2024-03-07 11:58:42 -0500

    Initial commit

---
+++ src/routes/v1/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userRoutes from "./user.route";
+
+
+
+
+const v1Router = express.Router();
+
+v1Router.use("/users", userRoutes);
+
+
+
+
+export default v1Router;
